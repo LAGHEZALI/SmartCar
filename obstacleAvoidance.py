27e0791edef13e8249aba2d460fba9d1ebb76e94
LAGHEZALI <laghezali.mohamed.reda@gmail.com>
2018-03-30 17:14:39 +0100

    c

--- obstacleAvoidance.py
+++ obstacleAvoidance.py
@@ -33,7 +33,7 @@ right_turn_sleep = 0.7
 safety_distance = 20
 max_angle = 180
 scan_list = []
-scan_list_size = 9
+scan_list_size = 5
 d_point = [50, 50]
 f_point = [50, 10000050]
 direction_goal = 0
@@ -56,7 +56,7 @@ servo.init()
 def scan():
     scan_list = []
     dis = 0.0
-    for i in range(10, 100, 10):
+    for i in range(30, 80, 5):
         servo.setServo(i)
         time.sleep(0.5)
         dis = us.getDistance()
